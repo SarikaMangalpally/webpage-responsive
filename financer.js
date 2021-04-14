9cb196edd7e785330023532bd968e0ec76455c20
Sarika <sathyamadipoju@gmail.com>
2021-04-14 10:49:54 +0530

    fixed few issues in navigation bar

--- financer.js
+++ financer.js
@@ -1,16 +1,25 @@
-function openNavBar() {
+// function openNavBar() {
+// }
+// function closeNavBar() {
+//   let menuBarIcon = document.getElementsByClassName('menu-bars')[0];
+//   let navbar = document.getElementsByTagName('nav')[0];
+//   let closeIcon = document.getElementsByClassName('close-icon')[0];
+// }
+function menubar() {
   let menuBarIcon = document.getElementsByClassName('menu-bars')[0];
   let navbar = document.getElementsByTagName('nav')[0];
   let closeIcon = document.getElementsByClassName('close-icon')[0];
-  menuBarIcon.style.visibility = "hidden";
-  navbar.style.visibility = 'visible';
-  closeIcon.style.visibility = 'visible';
+  let mainTag = document.getElementsByTagName('main')[0];
+  if (menuBarIcon.style.visibility == 'visible') {
+    menuBarIcon.style.visibility = "hidden";
+    navbar.style.visibility = 'visible';
+    closeIcon.style.visibility = 'visible';
+    mainTag.style.visibility = "hidden";
+  } else {
+    menuBarIcon.style.visibility = "visible";
+    navbar.style.visibility = "hidden";
+    closeIcon.style.visibility = 'hidden';
+    mainTag.style.visibility = "visible";
+  }
 }
-function closeNavBar() {
-  let menuBarIcon = document.getElementsByClassName('menu-bars')[0];
-  let navbar = document.getElementsByTagName('nav')[0];
-  let closeIcon = document.getElementsByClassName('close-icon')[0];
-  menuBarIcon.style.visibility = "visible";
-  navbar.style.visibility = "hidden";
-  closeIcon.style.visibility = 'hidden';
-}
+// function showSublistOfProducts() {}
